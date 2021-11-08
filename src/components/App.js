2fa5191670c9224ff6c4018d8a1ee26a6bb8ed5a
Jordan Yee <jordanyee799@gmail.com>
2021-11-07 22:54:30 -0800

    commit tree 2e758a9145988d95fcc9c19ac6f06acb8bf6ac13

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
+import {useEffect, useState} from 'react'
+
 // create your App component here
+const Url = 'https://dog.ceo/api/breeds/image/random'
+function App() {
+    const [image, setImage] = useState('Loading...')
+
+    useEffect(() => {
+        fetch(Url)
+        .then(res => res.json())
+        .then(json => setImage(json.message))
+    }, [])
+
+    return (
+        <>
+            {image ==='Loading...' ? <p>Loading...</p> : <img src={image} alt='A Random Dog'></img>}
+        </>
+    )
+    
+}
+
+export default App
